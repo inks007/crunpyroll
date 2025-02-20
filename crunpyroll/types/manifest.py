521e28190fa7d2792da60a45e82c26c210204b31
Pixel-LH <2569646547@qq.com>
2025-02-20 23:42:06 +0800

    fix for KeyError SegmentTemplate

--- crunpyroll/types/manifest.py
+++ crunpyroll/types/manifest.py
@@ -1,6 +1,6 @@
 from .obj import Object
 from .drm import ContentProtection
-
+from crunpyroll.types import SubtitlesStream
 from ..utils import (
     WIDEVINE_UUID,
     PLAYREADY_UUID,
@@ -10,6 +10,7 @@ from ..utils import (
 from typing import List, Dict
 
 import xmltodict
+
 
 class Manifest(Object):
     """
@@ -22,6 +23,9 @@ class Manifest(Object):
         audio_streams (List of :obj:`~crunpyroll.types.ManifestAudioStream`):
             List of every audio stream available.
 
+        subs_streams (List of :obj:`~crunpyroll.types.SubtitlesStream`):
+            List of every subtitle stream available
+
         content_protection (:obj:`~crunpyroll.types.ContentProtection`):
             Info about Content Protection (DRM).
 
@@ -29,9 +33,11 @@ class Manifest(Object):
             Plain version of the manifest (XML).
             Useful for external downloader tools.
     """
+
     def __init__(self, data: Dict):
         self.video_streams: List["ManifestVideoStream"] = data.get("video_streams")
         self.audio_streams: List["ManifestAudioStream"] = data.get("audio_streams")
+        self.sub_streams: List["SubtitlesStream"] = data.get("subs_streams")
         self.content_protection: "ContentProtection" = ContentProtection(data.get("content_protection"))
         self.plain: str = data.get("plain")
 
@@ -41,27 +47,36 @@ class Manifest(Object):
         data["plain"] = obj
         data["video_streams"] = []
         data["audio_streams"] = []
+        data["subs_streams"] = []
         data["content_protection"] = {}
         manifest = xmltodict.parse(obj)
         for aset in manifest["MPD"]["Period"]["AdaptationSet"]:
-            template = aset["SegmentTemplate"]
-            for drm in aset["ContentProtection"]:
-                scheme_id_uri = drm["@schemeIdUri"]
-                if scheme_id_uri == WIDEVINE_UUID:
-                    data["content_protection"]["widevine"] = {}
-                    data["content_protection"]["widevine"]["pssh"] = drm["cenc:pssh"]
-                    data["content_protection"]["widevine"]["key_id"] = drm["@cenc:default_KID"]
-                if scheme_id_uri == PLAYREADY_UUID:
-                    data["content_protection"]["playready"] = {}
-                    data["content_protection"]["playready"]["pssh"] = drm["mspr:pro"]
-            for repr in aset["Representation"]:
-                if repr.get("@mimeType").startswith("video"):
-                    stream = ManifestVideoStream.parse(repr, template)
-                    data["video_streams"].append(stream)
-                elif repr.get("@mimeType").startswith("audio"):
-                    stream = ManifestAudioStream.parse(repr, template)
+            if "SegmentTemplate" in aset:
+                template = aset["SegmentTemplate"]
+                for drm in aset["ContentProtection"]:
+                    scheme_id_uri = drm["@schemeIdUri"]
+                    if scheme_id_uri == WIDEVINE_UUID:
+                        data["content_protection"]["widevine"] = {}
+                        data["content_protection"]["widevine"]["pssh"] = drm["cenc:pssh"]
+                        data["content_protection"]["widevine"]["key_id"] = drm["@cenc:default_KID"]
+                    if scheme_id_uri == PLAYREADY_UUID:
+                        data["content_protection"]["playready"] = {}
+                        data["content_protection"]["playready"]["pssh"] = drm["mspr:pro"]
+                for repr in aset["Representation"]:
+                    if repr.get("@mimeType").startswith("video"):
+                        stream = ManifestVideoStream.parse(repr, template)
+                        data["video_streams"].append(stream)
+                    elif repr.get("@mimeType").startswith("audio"):
+                        stream = ManifestAudioStream.parse(repr, template)
                     data["audio_streams"].append(stream)
+            else:
+                mimeType = aset.get("@mimeType")
+                if mimeType.startswith("text/vtt"):
+                    repr = aset["Representation"]
+                    stream = SubtitlesStream(dict(format='vtt', language=aset["@lang"], url=repr["BaseURL"]))
+                    data["subs_streams"].append(stream)
         return cls(data)
+
 
 class ManifestVideoStream(Object):
     """
@@ -83,6 +98,7 @@ class ManifestVideoStream(Object):
         segments (List of ``str``):
             Each segment URL of the video stream.
     """
+
     def __init__(self, data: Dict):
         self.codecs: str = data.get("codecs")
         self.width: int = data.get("width")
@@ -99,7 +115,8 @@ class ManifestVideoStream(Object):
         data["bitrate"] = int(obj["@bandwidth"])
         data["segments"] = parse_segments(obj, template)
         return cls(data)
-    
+
+
 class ManifestAudioStream(Object):
     """
     Info about a manifest audio stream.
@@ -114,6 +131,7 @@ class ManifestAudioStream(Object):
         segments (List of ``str``):
             Each segment URL of the audio stream.
     """
+
     def __init__(self, data: Dict):
         self.codecs: str = data.get("codecs")
         self.bitrate: int = data.get("bitrate")
